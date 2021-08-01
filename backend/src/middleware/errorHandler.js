bdfbd3dca413cec552d320425cbf9c0d8441fc45
TheoCosmin <64136568+TheoCosmin@users.noreply.github.com>
2021-08-01 14:53:39 +0300

    added custom errors

---
+++ backend/src/middleware/errorHandler.js
@@ -0,0 +1,14 @@
+const CustomError = require('../errors/customError');
+
+const errorHandler = (err, req, res, next) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  console.log(err);
+
+  res.status(500).send({
+    errors: [{ message: 'Something went wrong' }],
+  });
+};
+
+module.exports = errorHandler;
